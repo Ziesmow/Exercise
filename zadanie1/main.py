e8c5690118fa7b53cf340207bb4babc57cf6f38f
Ziemowit <Ziesmow@proton.me>
2025-02-19 18:43:56 +0100

    zadanie 1 zrobione

--- zadanie1/main.py
+++ zadanie1/main.py
@@ -1,3 +1,39 @@
+a = 0
+b = -1
+c = 0
+reszta = []
+maxc = 0
+
 a = input("pobaj liczbę a: ")
 b = input("pobaj liczbę b: ")
 c = input("pobaj liczbę c: ")
+a = int(a)
+b = int(b)
+c = int(c)    
+liczby = [a, b, c]
+for liczba in liczby:
+    if liczba < 0 :
+        print("podaj liczby a, b, c  > 0")
+        a = input("pobaj liczbę a: ")
+        b = input("pobaj liczbę b: ")
+        c = input("pobaj liczbę c: ")
+        a = int(a)
+        b = int(b)
+        c = int(c)  
+        continue
+reszta = []
+maxc = a
+for lico in liczby:
+    
+    if lico > maxc:
+        maxc = lico
+reszta = []
+for resz in liczby:
+    if maxc == resz:
+        pass
+    else:
+        reszta.append(resz)
+i = 0
+while(i < maxc):
+    print(reszta[0] + reszta[1])
+    i += 1
