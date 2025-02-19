5b7a21ecbdd37ad726667630e01d3fc1f211adf5
Ziemowit <Ziesmow@proton.me>
2025-02-19 21:28:25 +0100

    zadanie 2 zrobione

---
+++ zadanie2/main.py
@@ -0,0 +1,5 @@
+a = int(input("podaj liczbę: "))
+if a % 2 == 0:
+    print(f"liczba {a} jest parzysta ")
+else: 
+    print(f"liczba {a} jest nieparzysta")
---
+++ zadanie2/readme.md
@@ -0,0 +1,2 @@
+Pobraną z klawiatury liczbę człkowitą zweryfikuj pod kontem parzystości.
+Wyświetl adekwatny komunikat, gdy jest lub nie jest parzysta.
