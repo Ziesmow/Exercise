2216fc1f030486f9dd5792a8e3813fe274f5206a
Ziemowit <Ziesmow@proton.me>
2025-02-20 09:20:01 +0100

    zadanie 3 zrobione

---
+++ zadanie3/main.py
@@ -0,0 +1,13 @@
+a = int(input("podaj liczbę całkowitą: "))
+if a % 3 == 0:
+    if a % 5 == 0:
+        print("liczba jest podzielna przez 3 i 5")
+    else:
+        print("liczba jest podzielna przez 3 i nie przez 5")
+if a % 5 != 0:
+    if a % 3 != 0:
+        print(" liczba nie jest przez ani 5 ani 3 podzielna")
+if a % 5 == 0:
+    if a % 3 != 0:
+        print("liczba nie jest podzielna przez 3 ale jest przez 5")
+
---
+++ zadanie3/readme.md
@@ -0,0 +1,1 @@
+Pobierz liczbę całkowitą z klawiatury i sprwadź, czy jest podzielna przez 3 i prez 5 i jej kompinacje        
