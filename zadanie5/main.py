f57e2bbc99701d34c4577058fa0ee5d9779feaaf
Ziemowit <Ziesmow@proton.me>
2025-02-22 16:48:55 +0100

    zadanie 5 zrobione

---
+++ zadanie5/main.py
@@ -0,0 +1,10 @@
+a1 = float(input("podaj liczbę: "))
+a2 = float(input("podaj liczbę: "))
+a3 = float(input("podaj liczbę: "))
+a4 = float(input("podaj liczbe: "))
+a5 = float(input("podaj liczbę: "))
+if a5 == 0:
+    print("nie moZna dzielić przez zero")
+else:
+    A = (((a1 + a2) * a3) - a4)/a5
+    print(f" wyrażenie wynosi {A}")
